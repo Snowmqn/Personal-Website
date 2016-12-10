4295b589c120f80ada94445f9b40d00ff3c3e593
Dayne Snow <Dayne.snow@gmail.com>
2016-12-10 11:04:51 -0700

    added server and yaml file

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).send('This actually worked!!!');
+});
+
+if (module === require.main) {
+  const server = app.listen(process.env.PORT || 8080, () => {
+    const port = server.address().port;
+    console.log(`listening on port ${port}`);
+  });
+}
+
+module.exports = app;
